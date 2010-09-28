49bfc549edf45bf67c6cbf62c4db97b71a5f5152
Andrew Stone <andrew@clovar.com>
2010-09-27 21:29:57 -0400

    Working towards something we can hack on:  * Makefile now supports input files and args for "run"  * Translator is composed of all the stuff from class.  * Updated README with new information.

--- Makefile
+++ Makefile
@@ -1,18 +1,30 @@
+#the classpath
+CLASSPATH=$(XTCROOT)/classes:.
+
 #the name of the file to run
-FILE=add
+ifdef file
+	FILE=test/$(file).java
+endif
+
+#args to be passed to the run command
+ARGS=
+ifdef args
+	ARGS=$(args)
+endif
 
 default: translator
 
 %.class: %.java
-	javac -source 1.5 -classpath $(XTCROOT)/classes $<
+	javac -source 1.5 -classpath $(CLASSPATH) $<
 	
 translator:
+	echo $(FILE)
 	@cd $(XTCROOT); . ./setup.sh; make > /dev/null #make sure we have xtc all running and setup
 	make `./make.srcs src`
 
-run: translator test/$(FILE).class
+run: translator $(FILE)
 	@echo "\n\n\n" #make some room from the compilation messages to the program output
-	@java src.Translator test/$(FILE).java
+	java -classpath $(CLASSPATH) src.Translator $(ARGS) $(FILE)
 
 clean-src:
 	find src -name "*.class" -exec rm {} \;
--- src/Translator.java
+++ src/Translator.java
@@ -1,8 +1,97 @@
 package src;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+import xtc.lang.JavaFiveParser;
+
+import xtc.parser.ParseException;
+import xtc.parser.Result;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
 import xtc.tree.Visitor;
 
-public class Translator {
+import xtc.util.Tool;
+
+public class Translator extends Tool {
+	/** Create a new translator. */
+	public Translator() {
+		// Nothing to do.
+	}
+
+	public String getCopy() {
+		return "(C) 2010 <Group Name>";
+	}
+
+	public String getName() {
+		return "Java to C++ Translator";
+	}
+
+	public String getExplanation() {
+		return "This tool translates a subset of Java to a subset of C++.";
+	}
+
+	public void init() {
+		super.init();
+
+		// Declare command line arguments.
+		runtime.
+			bool("printJavaAST", "printJavaAST", false, "Print the Java AST.").
+			bool("countMethods", "optionCountMethods", false, "Print the number of method declarations.")
+		;
+	}
+
+	public void prepare() {
+		super.prepare();
+		// Perform consistency checks on command line arguments.
+	}
+
+	public File locate(String name) throws IOException {
+		File file = super.locate(name);
+
+		if (Integer.MAX_VALUE < file.length())
+			throw new IllegalArgumentException(file + ": file too large");
+
+		return file;
+	}
+
+	public Node parse(Reader in, File file) throws IOException, ParseException {
+		JavaFiveParser parser = new JavaFiveParser(in, file.toString(), (int)file.length());
+		Result result = parser.pCompilationUnit(0);
+		return (Node)parser.value(result);
+	}
+
+	public void process(Node node) {
+		// Handle the printJavaAST option
+		if (runtime.test("printJavaAST"))
+			runtime.console().format(node).pln().flush();
+
+		if (runtime.test("optionCountMethods")) {
+			new Visitor() {
+				int count = 0;
+
+				public void visitCompilationUnit(GNode n) {
+					visit(n);
+					runtime.console().p("Number of methods: ").p(count).pln().flush();
+				}
+
+				public void visitMethodDeclaration(GNode n) {
+					count++;
+					visit(n);
+				}
+
+				public void visit(Node n) {
+					for (Object o : n) {
+						if (o instanceof Node)
+							dispatch((Node)o);
+					}
+				}
+			}.dispatch(node);
+		}
+	}
+
 	public static void main(String args []) {
 		System.out.println(args.length);
 		for (int i = 0; i < args.length; i++) {
@@ -10,5 +99,7 @@ public class Translator {
 		}
 		
 		System.out.println("Happy happy, joy joy!!");
+		
+		new Translator().run(args);
 	}
 }
