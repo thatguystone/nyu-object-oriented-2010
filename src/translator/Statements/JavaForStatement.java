73a29e0c93bf1e94426fc2a934938decd48f204d
Andrew Stone <andrew@clovar.com>
2010-11-10 19:50:04 -0500

    Fixing statements not compiling (go comments).

--- src/translator/Statements/JavaForStatement.java
+++ src/translator/Statements/JavaForStatement.java
@@ -1,4 +1,4 @@
-package translator;
+package translator.Statements;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -6,22 +6,23 @@ import java.util.LinkedHashMap;
 import xtc.tree.Node;
 import xtc.tree.GNode;
 
-class JavaForStatement extends JavaScope{
-	private String type;  /** type of the field **/
+class JavaForStatement { /* extends JavaScope{
+	private String type;  /** type of the field **
 	JavaForStatement(JavaScope scope, GNode n){
 		super(scope, n);
 		this.dispatch(n);
 	}
 	protected void onInstantiate(GNode n){
-		/** may not need to do anything here **/
+		/** may not need to do anything here **
 	}
 	public void visitBasicForControl(GNode n){
-		/** first attempt to "normalize" for control **/
-		/** if there is any initialization, then initialization(s) can actually be treated the same as field dec **/
+		/** first attempt to "normalize" for control **
+		/** if there is any initialization, then initialization(s) can actually be treated the same as field dec **
 		if(n.get(0)!=null){
-			new JavaForInit(this,n);
+			//new JavaForInit(this,n);
 		}else{
-			/** will fill in other details later **/
+			/** will fill in other details later **
 		}
 	}
+	*/
 }
