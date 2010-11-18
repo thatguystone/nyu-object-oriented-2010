ee97a0715c5a792b9e38f7e93b95d84ed2714858
vcsubman <vcsubman@nyu.(none)>
2010-11-17 19:48:27 -0500

    tentative framework for all statements, and fixing bugs in for statement

--- src/translator/Statements/JavaForStatement.java
+++ src/translator/Statements/JavaForStatement.java
@@ -1,18 +1,18 @@
 package translator.Statements;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-
 import xtc.tree.Node;
 import xtc.tree.GNode;
 
-class JavaForStatement  extends JavaScope{
-	JavaBasicForControl ctrl;
+class JavaForStatement extends JavaStatement{
 	JavaForStatement(JavaScope scope, GNode n){
 		super(scope, n);
-		this.dispatch(n);
 	}
-	public void visitBasicForControl(GNode n){
-		ctrl=new JavaBasicForControl(this,n);
+	public JavaBasicForControl visitBasicForControl(GNode n){
+		return new JavaBasicForControl(this,n);
+	}
+	public CodeBlock printMe(CodeBlock block){
+		return new CodeBlock(block,"for (" + header.printMe + ")",true).close();  /** THEN WHERE IS THE BEEF?? **/
+		/** i.e. what to do with the printing of subscope(i) **/
 	}
 }
--- src/translator/Statements/JavaStatement.java
+++ src/translator/Statements/JavaStatement.java
@@ -11,7 +11,19 @@ class JavaStatement extends JavaScope {
 	 * SAEKJFA;WIE JF K;LSDFJ ASILD JFASD;IFJ!!!!!!! WHY DOES JAVA NOT INHERIT CONSTRUCTORS?!?!?!?!?!?!?!?!?!??!
 	 * This feels so dirty and wrong.
 	 */
+	JavaScope header;
+	ArrayList<JavaScope> subscope = newArrayList<JavaScope>();
 	JavaStatement(JavaScope scope, GNode n) {
 		super(scope, n);
+		header=n.dispatch(0);
+		for (int i=1;i<n.size();++i){
+			final GNode g=(GNode)n.get(i);
+			if (g!=null){
+				subscope.add((JavaScope)n.dispatch(g));
+			}
+		}
+	}
+	CodeBlock printMe(CodeBlock block){
+		return null;
 	}
 }
