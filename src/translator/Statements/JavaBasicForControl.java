3445f78befb2fe74f234c2716615c13a0107f896
vcsubman <vcsubman@nyu.(none)>
2010-11-18 13:36:11 -0500

    fixed errors

--- src/translator/Statements/JavaBasicForControl.java
+++ src/translator/Statements/JavaBasicForControl.java
@@ -1,35 +1,31 @@
-/*
+
 package translator.Statements;
 
 import xtc.tree.Node;
 import xtc.tree.GNode;
 import translator.FieldDec;
 import translator.JavaField;
-
+import translator.JavaScope;
 class JavaBasicForControl extends JavaScope{
 	JavaScope ForInit;
 	JavaScope ForExpression;
 	JavaScope ForUpdate;
-	JavaBasicForControl (JavaScope scope, Node n) {
+	JavaBasicForControl (JavaScope scope, GNode n) {
 		super(scope,n);
 	}
 	protected void onInstantiate(GNode n){
 		if (n.get(3)!=null){
-			ForExpression=dispatch((GNode)n.set(3,null));
+			ForExpression=(JavaScope)dispatch((GNode)n.set(3,null));
 		}
 		if (n.get(4)!=null){
-			ForUpdate=dispatch((GNode)n.set(4,null));
+			ForUpdate=(JavaScope)dispatch((GNode)n.set(4,null));
 		}
 		if (n.get(1)!=null){  /** if this node has a type **/
-		/*	new FieldDec(this,n){
-				public void visitDeclarator(GNode n){
-					/** n is now a field dec node with 2 extra nulls appended**/
-					/*ForInit=new JavaField(false, this.modifiers, this.type, this.dimensions, this, n);
-				}
-			};
+			new FieldDec(this, n);  /** assuming FieldDec adds field to this.field, then the for loop initialization
+						    would be just printed as a field, and ForInit would be null and would not be printed**/
 		}else{  /** otherwise it is a single expression or expression list (assuming JavaScope will add field to parent scope) **/
-			/*ForInit=n.dispatch(get(2));
+			ForInit=(JavaScope)dispatch((GNode)n.get(2));
 		}
 	}
 }
-*/
+
--- src/translator/Statements/JavaStatement.java
+++ src/translator/Statements/JavaStatement.java
@@ -1,25 +1,28 @@
+
 package translator.Statements;
-
 import translator.JavaScope;
+import translator.Printer.CodeBlock;
+import java.util.ArrayList;
 import xtc.tree.GNode;
-
 /**
  * Handles anything that can have its own scope, from a File to a Block.
  */
+
 class JavaStatement extends JavaScope {
 	/**
 	 * SAEKJFA;WIE JF K;LSDFJ ASILD JFASD;IFJ!!!!!!! WHY DOES JAVA NOT INHERIT CONSTRUCTORS?!?!?!?!?!?!?!?!?!??!
 	 * This feels so dirty and wrong.
 	 */
+
 	JavaScope header;
-	ArrayList<JavaScope> subscope = newArrayList<JavaScope>();
+	ArrayList<JavaScope> subscope = new ArrayList<JavaScope>();
 	JavaStatement(JavaScope scope, GNode n) {
 		super(scope, n);
-		header=n.dispatch(0);
+		header=(JavaScope)dispatch((GNode)n.get(0));
 		for (int i=1;i<n.size();++i){
 			final GNode g=(GNode)n.get(i);
 			if (g!=null){
-				subscope.add((JavaScope)n.dispatch(g));
+				subscope.add((JavaScope)dispatch(g));
 			}
 		}
 	}
@@ -27,3 +30,4 @@ class JavaStatement extends JavaScope {
 		return null;
 	}
 }
+
