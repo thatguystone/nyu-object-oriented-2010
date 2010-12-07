24b4ec60a074ac2d668653f182b627997af9c592
vcsubman <vcsubman@nyu.(none)>
2010-12-07 14:25:52 -0500

    fixed some errors, ForInit is printing correctly, and some minor changes

--- src/translator/JavaScope.java
+++ src/translator/JavaScope.java
@@ -196,6 +196,7 @@ public class JavaScope extends Visitor {
 
 	public JavaStatement visitForStatement(GNode n) {
 		return new ForStatement(this, n);
+//		return new JavaForStatement(this, n);
 	}
 
 	public JavaStatement visitWhileStatement(GNode n) {
