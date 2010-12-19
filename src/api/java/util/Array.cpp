0cfc6059329de680fe1b8b6bb588a85723c525d8
vcsubman <vcsubman@nyu.(none)>
2010-12-19 10:16:19 -0500

    still trying to fix it

--- src/api/java/util/Array.cpp
+++ src/api/java/util/Array.cpp
@@ -7,18 +7,20 @@ T& __Array<T>::get(int32_t dim, ...) {
 	va_list args; 
 	va_start(args, dim);
 	
-	ARRAY(T)* find = __data;
-	
 	int32_t d;
-	for (int32_t i = 0; i < dim-2; i++) {
+	if (dim>1){
+		ARRAY(T)* find = __data;
+		for (int32_t i = 0; i < dim-1; ++i) {
+			d = va_arg(args, int32_t);
+			//if we're accessing something beyond our reach
+			if (d < 0 || d > __dims[i]-1)
+				throw java::lang::ArrayIndexOutOfBoundsException();
+			find = (find[d])->__data;
+		}
 		d = va_arg(args, int32_t);
-		//if we're accessing something beyond our reach
-		if (d < 0 || d > __dims[i]-1)
-			throw java::lang::ArrayIndexOutOfBoundsException();
-		find = (find[d])->__data;
+		return (*find)->__arrayData[d];
 	}
-	d = va_arg(args, int32_t);
-	return (*find)->__arrayData[d];
+	return __arrayData[d];
 }
 
 template <typename T>
